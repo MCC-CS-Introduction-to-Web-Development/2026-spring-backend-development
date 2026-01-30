df87054a3ed9399c836cb9d3f24f7c0564095ba3
Evan Stohlmann <estohlmann@gmail.com>
2026-01-29 18:44:07 -0700

    class 2

--- class2/Hello.java
+++ class2/Hello.java
@@ -1,7 +1,22 @@
 package class2;
 
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String... args){
         System.out.println("Hello world");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your favorite color: ");
+        String color = input.nextLine();
+
+        System.out.print("Enter your favorite number: ");
+        String number = input.nextLine();
+
+        String random = color + " " + number;
+
+        System.out.println();
+        System.out.println("\nI really like random things: " + random);
+        System.out.printf("Your favorite color is %s!", color);
     }
 }
