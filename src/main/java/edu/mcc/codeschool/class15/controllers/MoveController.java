65742aad919293caaa5810786734827dde118ae2
Evan Stohlmann <estohlmann@gmail.com>
2026-03-05 19:43:30 -0700

    Class 15

---
+++ src/main/java/edu/mcc/codeschool/class15/controllers/MoveController.java
@@ -0,0 +1,35 @@
+package edu.mcc.codeschool.class15.controllers;
+
+import edu.mcc.codeschool.class15.models.Game;
+import edu.mcc.codeschool.class15.models.Move;
+import edu.mcc.codeschool.class15.services.MoveService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/moves")
+public class MoveController {
+    private final MoveService moveService;
+
+    public MoveController(MoveService moveService) {
+        this.moveService = moveService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> makeMove(@RequestBody @Valid Move request){
+        try {
+            Game response = moveService.makeMove(request);
+            return response != null ? ResponseEntity.ok(response) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e){
+            if(e.getMessage().equals("Game has winner")){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
