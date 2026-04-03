1c0200103856a281929d33541a34d1774601a4b8
Evan Stohlmann <estohlmann@gmail.com>
2026-04-02 18:38:07 -0600

    Class 25

--- src/main/java/edu/mcc/codeschool/class25/controllers/GameController.java
+++ src/main/java/edu/mcc/codeschool/class25/controllers/GameController.java
@@ -1,7 +1,14 @@
 package edu.mcc.codeschool.class25.controllers;
 
 import edu.mcc.codeschool.class22.models.Game;
+import edu.mcc.codeschool.class25.models.Move;
 import edu.mcc.codeschool.class25.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,14 +23,45 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/games")
+@Tag(name = "Game APIs")
 public class GameController {
     private final GameService gameService;
+
+    private final String EXAMPLE_GAME_BOARD =  "{\n" +
+            "  \"id\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n" +
+            "  \"status\": \"In-Progress\",\n" +
+            "  \"winner\": null,\n" +
+            "  \"playerXWins\": 0,\n" +
+            "  \"playerOWins\": 0,\n" +
+            "  \"ties\": 0,\n" +
+            "  \"board\": [\n" +
+            "    [\n" +
+            "      [\"\", \"\", \"\"],\n" +
+            "      [\"\", \"X\", \"\"],\n" +
+            "      [\"\", \"\", \"\"]\n" +
+            "    ]\n" +
+            "  ]\n" +
+            "}";
 
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
 
     @PostMapping
+    @Operation(
+            description = "Get all games",
+            summary = "API to list all active games",
+            responses = {
+                    @ApiResponse(
+                            description = "Success Response",
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Game.class, example ="[\n" + EXAMPLE_GAME_BOARD + "\n]")
+                            )
+                    )
+            }
+    )
     public ResponseEntity<Game> createGame(){
         Game result = gameService.createGame();
         return ResponseEntity.ok(result);
@@ -40,6 +78,33 @@ public class GameController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            description = "Get Game by ID",
+            summary = "This endpoint returns a game, if found, by it's ID",
+            parameters = @Parameter(
+              name = "id",
+              description = "The id of the game you want to find",
+              example =  "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+            ),
+            responses = {
+                    @ApiResponse(
+                            description = "Success Response",
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Game.class, example = EXAMPLE_GAME_BOARD)
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Game not Found Response",
+                            responseCode = "404",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = " ")
+                            )
+                    )
+            }
+    )
     public ResponseEntity<Game> getGameById(@PathVariable String id){
         Optional<Game> game =  gameService.getGameById(id);
         return game.isPresent() ? ResponseEntity.ok(game.get()) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- src/main/java/edu/mcc/codeschool/class25/controllers/MoveController.java
+++ src/main/java/edu/mcc/codeschool/class25/controllers/MoveController.java
@@ -3,6 +3,11 @@ package edu.mcc.codeschool.class25.controllers;
 import edu.mcc.codeschool.class22.models.Game;
 import edu.mcc.codeschool.class25.models.Move;
 import edu.mcc.codeschool.class25.services.MoveService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/moves")
+@Tag(name = "Move APIs")
 public class MoveController {
     private final MoveService moveService;
 
@@ -21,6 +27,61 @@ public class MoveController {
     }
 
     @PostMapping
+    @Operation(
+            description = "Make a move",
+            summary = "This endpoint makes a move in an active game",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "JSON Body",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Move.class, example = "{\n" +
+            "  \"gameId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n" +
+            "  \"location\": \"1,1\",\n" +
+            "  \"player\": \"X\"\n" +
+            "}")
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            description = "Success Response",
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Game.class, example = "{\n" +
+                                            "  \"id\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n" +
+                                            "  \"status\": \"In-Progress\",\n" +
+                                            "  \"winner\": null,\n" +
+                                            "  \"playerXWins\": 0,\n" +
+                                            "  \"playerOWins\": 0,\n" +
+                                            "  \"ties\": 0,\n" +
+                                            "  \"board\": [\n" +
+                                            "    [\n" +
+                                            "      [\"\", \"\", \"\"],\n" +
+                                            "      [\"\", \"X\", \"\"],\n" +
+                                            "      [\"\", \"\", \"\"]\n" +
+                                            "    ]\n" +
+                                            "  ]\n" +
+                                            "}")
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Game not Found Response",
+                            responseCode = "404",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = " ")
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Bad Request Response",
+                            responseCode = "400",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = " ")
+                            )
+                    ),
+            }
+    )
     public ResponseEntity<Game> makeMove(@RequestBody @Valid Move request){
         try {
             Game response = moveService.makeMove(request);
